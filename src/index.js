f5032e4c73ccd4e5839dc27a3cc4300db299b94e
Vadym <vzhufk@gmail.com>
2018-06-09 12:35:11 +0300

    - Test.  - Throws.  - Options.

--- src/index.js
+++ src/index.js
@@ -24,10 +24,19 @@ for (let i in FILTER_OPERATORS) {
 /**
  * Export the plugin.
  */
-const bcqm = (Bookshelf, options) => {
+const bcqm = (
+  Bookshelf,
+  options = {
+    filterName: "filter",
+    orderByName: "orderBy",
+    hiddenPropsName: "hidden",
+    allowedPropsName: "allowed",
+    ignoreErrors: false
+  }
+) => {
   const Model = Bookshelf.Model.prototype;
   Bookshelf.Model = Bookshelf.Model.extend({
-    filter(expression) {
+    [options.filterName]: function(expression) {
       if (!expression) {
         return this;
       }
@@ -35,33 +44,48 @@ const bcqm = (Bookshelf, options) => {
       // Replacing proper logical operations
       expression = expression.replace(/ and /g, " && ");
       expression = expression.replace(/ or /g, " || ");
-      const props = this.props;
-      const hidden = this.hidden;
+
+      const allowed = this[options.allowedPropsName];
+      const hidden = this[options.hiddenPropsName];
 
       function split(node) {
-        if (node.type === "BinaryExpression") {
-          let field = node.left.name;
-          let operation = FILTER_OPERATORS[node.operator];
-          let value = node.right.value;
+        try {
+          if (node.type === "BinaryExpression") {
+            let field = node.left.name;
+            let operation = FILTER_OPERATORS[node.operator];
+            let value = node.right.value;
 
-          if (!operation) {
-            throw new Error("This operation is not available.");
-          }
-
-          return qb => {
-            qb.where(field, operation, value);
-          };
-        } else if (node.type === "LogicalExpression") {
-          return qb => {
-            qb.where(split(node.left));
-            if (node.operator === "&&") {
-              qb.andWhere(split(node.right));
-            } else if (node.operator === "||") {
-              qb.orWhere(split(node.right));
+            if (!operation) {
+              throw new Error(`'${node.operator}' operation is not available.`);
             }
-          };
-        } else {
-          throw new Error("Failed to parse to Logical Expression.");
+
+            if (hidden && hidden.includes(field)) {
+              throw new Error(`'${field}' is hidden in Model.`);
+            }
+
+            if (allowed && !allowed.includes(field)) {
+              throw new Error(`'${field}' is not allowed in Model.`);
+            }
+
+            return qb => {
+              qb.where(field, operation, value);
+            };
+          } else if (node.type === "LogicalExpression") {
+            return qb => {
+              qb.where(split(node.left));
+              if (node.operator === "&&") {
+                qb.andWhere(split(node.right));
+              } else if (node.operator === "||") {
+                qb.orWhere(split(node.right));
+              }
+            };
+          } else {
+            throw new Error(`Failed to parse to Logical Expression.`);
+          }
+        } catch (e) {
+          if (!options.ignoreErrors) {
+            throw e;
+          }
         }
       }
 
@@ -70,19 +94,36 @@ const bcqm = (Bookshelf, options) => {
       return this.query(split(tree));
     },
 
-    order(expression) {
+    [options.orderByName]: function(expression) {
       if (!expression) {
         return this;
       }
-      const props = this.props;
-      const hidden = this.hidden;
+      const allowed = this[options.allowedPropsName];
+      const hidden = this[options.hiddenPropsName];
+
       expression = expression.split(", ");
+
       for (let part of expression) {
-        part = part.split(" ");
-        let field = part[0];
-        let direction =
-          (part[1] && ORDER_DIRECTION[part[1]]) || ORDER_DIRECTION.asc;
-        this.query(qb => qb.orderBy(field, direction));
+        try {
+          part = part.split(" ");
+          let field = part[0];
+          let direction =
+            (part[1] && ORDER_DIRECTION[part[1]]) || ORDER_DIRECTION.asc;
+
+          if (hidden && hidden.includes(field)) {
+            throw new Error(`'${field}' is hidden in Model.`);
+          }
+
+          if (allowed && !allowed.includes(field)) {
+            throw new Error(`'${field}' is not allowed in Model.`);
+          }
+
+          this.query(qb => qb.orderBy(field, direction));
+        } catch (e) {
+          if (!options.ignoreErrors) {
+            throw e;
+          }
+        }
       }
       return this;
     }
--- test/index.test.js
+++ test/index.test.js
@@ -4,14 +4,16 @@ const bookshelf = require("bookshelf");
 const knex = require("knex");
 const sqlite3 = require("sqlite3");
 
+const fs = require("fs");
+
 describe("bookshelf-collection-query-mutation", () => {
-  const db = new sqlite3.Database("./test.sqlite3");
+  const db = new sqlite3.Database("./test/test.sqlite3");
   const repository = bookshelf(
     knex({
       client: "sqlite3",
       useNullAsDefault: true,
       connection: {
-        filename: "./test.sqlite3"
+        filename: "./test/test.sqlite3"
       }
     })
   );
@@ -19,13 +21,13 @@ describe("bookshelf-collection-query-mutation", () => {
   repository.plugin(bcqm);
 
   const data = [
-    { id: 1, name: "Eggs", price: 0.5, qty: 10 },
-    { id: 2, name: "Milk", price: 2.5, qty: 2 },
-    { id: 3, name: "Bread", price: 2, qty: 1 },
-    { id: 4, name: "Apple", price: 1, qty: 3 },
-    { id: 5, name: "Banana", price: 1.5, qty: 2 },
-    { id: 6, name: "Spam", price: 5, qty: 1 },
-    { id: 7, name: "Salt", price: 2.78, qty: 1 }
+    { id: 1, name: "Eggs", price: 0.5, qty: 10, barcode: "EGG432141" },
+    { id: 2, name: "Milk", price: 2.5, qty: 2, barcode: "MLK957212" },
+    { id: 3, name: "Bread", price: 2, qty: 1, barcode: "BRD226943" },
+    { id: 4, name: "Apple", price: 1, qty: 3, barcode: "APL230010" },
+    { id: 5, name: "Banana", price: 1.5, qty: 2, barcode: "BNA715315" },
+    { id: 6, name: "Spam", price: 5, qty: 1, barcode: "SPM239742" },
+    { id: 7, name: "Salt", price: 2.78, qty: 1, barcode: "SLT110023" }
   ];
 
   beforeAll(async () => {
@@ -35,12 +37,15 @@ describe("bookshelf-collection-query-mutation", () => {
       table.string("name");
       table.integer("qty");
       table.float("price");
+      table.string("barcode");
     });
     await repository.knex("shopping_list").insert(data);
   });
 
   const List = repository.Model.extend({
-    tableName: "shopping_list"
+    tableName: "shopping_list",
+    allowed: ["name", "qty", "price"],
+    hidden: ["barcode"]
   });
 
   describe("filter", () => {
@@ -70,6 +75,17 @@ describe("bookshelf-collection-query-mutation", () => {
       expect(items).toBe(
         data.filter(v => (v.qty >= 2 && v.price >= 2) || v.name == "Eggs")
           .length
+      );
+    });
+
+    test("select items (qty >= 2 and price >= 2) or (qty < 2 and price < 2)", async () => {
+      let items = List.forge();
+      items.filter(`(qty ge 2 and price ge 2) or (qty lt 2 and price lt 2)`);
+      items = await items.count();
+      expect(items).toBe(
+        data.filter(
+          v => (v.qty >= 2 && v.price >= 2) || (v.qty < 2 && v.price < 2)
+        ).length
       );
     });
   });
@@ -104,7 +120,104 @@ describe("bookshelf-collection-query-mutation", () => {
         .fetch();
       expect(items.get("qty")).toBe(data.sort((a, b) => a.qty > b.qty)[0].qty);
     });
+
+    test("select one order by name, qty, price", async () => {
+      let items = await List.forge()
+        .orderBy("name, qty, price")
+        .fetch();
+      expect(items.get("id")).toBe(
+        data
+          .sort((a, b) => a.price > b.price)
+          .sort((a, b) => a.qty > b.qty)
+          .sort((a, b) => a.name > b.name)[0].id
+      );
+    });
   });
 
-  describe("filter and orderBy", () => {});
+  describe("filter and orderBy", () => {
+    test("select one qty >= 2 orderBy price", async () => {
+      let items = await new List()
+        .filter("qty ge 2")
+        .orderBy("price")
+        .fetch();
+      expect(items.get("id")).toBe(
+        data.filter(v => v.qty >= 2).sort((a, b) => a.price > b.price)[0].id
+      );
+    });
+
+    test("select one qty >= 2 and price >= 2 orderBy price", async () => {
+      let items = await new List()
+        .filter("qty ge 2 and price ge 2")
+        .orderBy("price")
+        .fetch();
+      expect(items.get("id")).toBe(
+        data
+          .filter(v => v.qty >= 2 && v.price >= 2)
+          .sort((a, b) => a.price > b.price)[0].id
+      );
+    });
+
+    test("select one price >= 2 orderBy name, price", async () => {
+      let items = await new List()
+        .filter("price ge 2")
+        .orderBy("name, price")
+        .fetch();
+      expect(items.get("id")).toBe(
+        data
+          .filter(v => v.price >= 2)
+          .sort((a, b) => a.price > b.price)
+          .sort((a, b) => a.name > b.name)[0].id
+      );
+    });
+  });
+
+  describe("errors", () => {
+    test("error", () => {
+      expect(() => {
+        throw new Error("Test.");
+      }).toThrow("Test");
+    });
+
+    test("bad filter expression", () => {
+      let items = new List();
+      expect(() => items.filter("price 10")).toThrow(
+        "Failed to parse to Logical Expression."
+      );
+    });
+
+    test("bad filter operation in expression", () => {
+      let items = new List();
+      expect(() => items.filter("price le 10 and qty === 1")).toThrow(
+        "'===' operation is not available."
+      );
+    });
+
+    test("hidden filter property", () => {
+      let items = new List();
+      expect(() =>
+        items.filter("price le 10 and barcode il EGG432141")
+      ).toThrow("'barcode' is hidden in Model.");
+    });
+
+    test("not allowed filter property", () => {
+      let items = new List();
+      expect(() => items.filter("price le 10 and id ge 1")).toThrow(
+        "'id' is not allowed in Model."
+      );
+    });
+
+    test("hidden orderBy property", () => {
+      let items = new List();
+      expect(() => items.orderBy("price, barcode")).toThrow(
+        "'barcode' is hidden in Model."
+      );
+    });
+
+    test("not allowed orderBy property", () => {
+      let items = new List();
+      expect(() => items.orderBy("price, id")).toThrow(
+        "'id' is not allowed in Model."
+      );
+    });
+  });
 });
